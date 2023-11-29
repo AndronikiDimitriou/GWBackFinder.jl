8e3d292a298062951d399706129939835225d613
AndronikiDimitriou <androniki.dimitriou@ific.uv.es>
2023-11-29 20:20:12 +0100

    "up"

--- dev/choose_signal.jl
+++ dev/choose_signal.jl
@@ -9,8 +9,6 @@ f = range(start=3 * 1e-5, stop=0.5, step=1e-6)
 idx,idx27,logbins_27,logbins,f_filtered = GWBackFinder.binning(f)
 Sb1_new3, Sb2_new3, Sb3_new3, Sb4_new3, Sb5_new3, Sb6_new3, Sb7_new3, Sb8_new3, Sb9_new3, Sb10_new3, Sb11_new3, Sb12_new3, Sb13_new3, Sb14_new3, Sb15_new3, Sb16_new3, Sb17_new3, Sb18_new3, Sb19_new3, Sb20_new3, Sb21_new3, Sb22_new3, Sb23_new3, Sb24_new3, Sb25_new3, Sb26_new3, Sb27_new3 = logbins_27[1], logbins_27[2], logbins_27[3], logbins_27[4], logbins_27[5], logbins_27[6], logbins_27[7], logbins_27[8], logbins_27[9], logbins_27[10], logbins_27[11], logbins_27[12], logbins_27[13], logbins_27[14], logbins_27[15], logbins_27[16], logbins_27[17], logbins_27[18], logbins_27[19], logbins_27[20], logbins_27[21], logbins_27[22], logbins_27[23], logbins_27[24], logbins_27[25], logbins_27[26], logbins_27[27]
 
-length(logbins_27)
-
 z_power_law_test         =  GWBackFinder.zPowerLaw([-9,0.6])
 z_peak_test              =  GWBackFinder.zPeak([-9.,0.002,0.2])
 z_wiggly_test            =  GWBackFinder.zWiggly([-10,2,0.1])
--- dev/generate_mock_signal.jl
+++ dev/generate_mock_signal.jl
@@ -9,23 +9,16 @@ f = range(start=3 * 1e-5, stop=0.5, step=1e-6)
 idx,idx27,logbins_27,logbins,f_filtered=GWBackFinder.binning(f)
 Sb1_new3, Sb2_new3, Sb3_new3, Sb4_new3, Sb5_new3, Sb6_new3, Sb7_new3, Sb8_new3, Sb9_new3, Sb10_new3, Sb11_new3, Sb12_new3, Sb13_new3, Sb14_new3, Sb15_new3, Sb16_new3, Sb17_new3, Sb18_new3, Sb19_new3, Sb20_new3, Sb21_new3, Sb22_new3, Sb23_new3, Sb24_new3, Sb25_new3, Sb26_new3, Sb27_new3 = logbins_27[1], logbins_27[2], logbins_27[3], logbins_27[4], logbins_27[5], logbins_27[6], logbins_27[7], logbins_27[8], logbins_27[9], logbins_27[10], logbins_27[11], logbins_27[12], logbins_27[13], logbins_27[14], logbins_27[15], logbins_27[16], logbins_27[17], logbins_27[18], logbins_27[19], logbins_27[20], logbins_27[21], logbins_27[22], logbins_27[23], logbins_27[24], logbins_27[25], logbins_27[26], logbins_27[27]
 
-idx
 
-z1=vcat(rand(29),3)
-
-
-z1=[0.1,0.5,0.2,0.6,0.3,0.7,0.4,0.9,0.3,0.8,0.1,0.5,0.2,0.6,0.3,0.7,0.4,0.9,0.3,0.8,0.1,0.5,0.2,0.6,0.3,0.7,0.4,0.9,3]
-
-length(z1)
-
+z1=vcat(rand(28),3)
 z1=[0.525,0.525,0.525,0.525,0.525,0.525,0.525,0.525,0.525,0.525,0.525,0.525,0.525,0.525,0.525,0.525,0.525,0.525,0.525,0.525,0.525,0.525,0.525,0.525,0.525,0.525,0.525,0.525,3]
-length(z1)
 data,freq,Data=GWBackFinder.model(z1,f,"AA",idx,f_filtered,logbins_27,logbins, Sb1_new3, Sb2_new3, Sb3_new3, Sb4_new3, Sb5_new3, Sb6_new3, Sb7_new3, Sb8_new3, Sb9_new3, Sb10_new3, Sb11_new3, Sb12_new3, Sb13_new3, Sb14_new3, Sb15_new3, Sb16_new3, Sb17_new3, Sb18_new3, Sb19_new3, Sb20_new3, Sb21_new3, Sb22_new3, Sb23_new3, Sb24_new3, Sb25_new3, Sb26_new3)
 
-length(data[1][1])
 plt.clf()
 plt.plot(freq, data[1][1])
 plt.plot(log10.(f),Data)
+plt.axvline(log10(Sb14_new3))
+plt.axhline(log10(10^(-14+0.525*(-6-(-14)))))
 
 """
 plt.axvline(log10(Sb1_new3))
