c91ba52a81d1bb6917e30c300f2f47391d18e481
AndronikiDimitriou <androniki.dimitriou@ific.uv.es>
2023-12-04 01:46:30 +0100

    "up"

--- src/binning.jl
+++ src/binning.jl
@@ -1,11 +1,12 @@
 
 function binning(f)
+
     ## 1000 bins
     f_filtered = f[971:end]
     logbins = 10 .^ range(log10(0.001), stop=log10(0.5), length=1000)
     idx = [searchsortedlast(logbins, f_filtered[i]) for i in eachindex(f_filtered)]
 
-    ## 28 bins
+    ## 27 bins
     logbins_27 = 10 .^ range(log10(3 * 1e-5), stop=log10(0.5), length=27)
     idx_27 = [searchsortedlast(logbins_27, f[i]) for i in eachindex(f)]
     idx_27[1]=1
