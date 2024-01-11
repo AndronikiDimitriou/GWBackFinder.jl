a7610e654eac8460db5bae872a0e51b2a85c1e96
AndronikiDimitriou <androniki.dimitriou@ific.uv.es>
2024-01-12 00:09:53 +0100

    "up"

--- examples/generate_mock_signal.jl
+++ examples/generate_mock_signal.jl
@@ -1,7 +1,6 @@
 using Pkg
 using Revise
-Pkg.activate("./")
-#Pkg.activate("/home/zaldivar/Documents/Androniki/Github/GWBackFinder.jl")
+Pkg.activate("/home/zaldivar/Documents/Androniki/Github/GWBackFinder.jl")
 using GWBackFinder
 using PyPlot
 using NPZ
@@ -16,19 +15,20 @@ f = range(start=3 * 1e-5, stop=0.5, step=1e-6)
 idx,idx26,logbins_26,logbins,f_filtered=GWBackFinder.binning(f)
 
 ### Define breakpoints, i.e., read the boundaries of the 26 bins.
-Sb1, Sb2, Sb3, Sb4, Sb5, Sb6, Sb7, Sb8, Sb9, Sb10, Sb11, Sb12, Sb13, Sb14, Sb15, Sb16, Sb17, Sb18, Sb19, Sb20, Sb21, Sb22, Sb23, Sb24, Sb25, Sb26 = logbins_26[1], logbins_26[2], logbins_26[3], logbins_26[4], logbins_26[5], logbins_26[6], logbins_26[7], logbins_26[8], logbins_26[9], logbins_26[10], logbins_26[11], logbins_26[12], logbins_26[13], logbins_26[14], logbins_26[15], logbins_26[16], logbins_26[17], logbins_26[18], logbins_26[19], logbins_26[20], logbins_26[21], logbins_26[22], logbins_26[23], logbins_26[24], logbins_26[25], logbins_26[26]
+Sb1, Sb2, Sb3, Sb4, Sb5, Sb6, Sb7, Sb8, Sb9, Sb10, Sb11, Sb12, Sb13, Sb14, Sb15, Sb16, Sb17, Sb18, Sb19, Sb20, Sb21, Sb22, Sb23, Sb24, Sb25= logbins_26[1], logbins_26[2], logbins_26[3], logbins_26[4], logbins_26[5], logbins_26[6], logbins_26[7], logbins_26[8], logbins_26[9], logbins_26[10], logbins_26[11], logbins_26[12], logbins_26[13], logbins_26[14], logbins_26[15], logbins_26[16], logbins_26[17], logbins_26[18], logbins_26[19], logbins_26[20], logbins_26[21], logbins_26[22], logbins_26[23], logbins_26[24], logbins_26[25]
 
-### Define values for parameters randomly. 10000000 different z (z -> 26 slopes, 1 amplitude, 1 noise parameter A) 
+### Define values for parameters randomly. 10000000 different z (z -> 27 slopes, 1 amplitude, 1 noise parameter A) 
 z= npzread("/data/users/Androniki/Dani/z_new.npy")
 
 ### Look at an example and plot it 
-data,freq=GWBackFinder.model(z[5,:],f,idx,f_filtered,logbins_26,logbins, Sb1, Sb2  , Sb3  , Sb4  , Sb5  , Sb6  , Sb7  , Sb8  , Sb9  , Sb10  , Sb11  , Sb12  , Sb13  , Sb14  , Sb15  , Sb16  , Sb17  , Sb18  , Sb19  , Sb20  , Sb21  , Sb22  , Sb23  , Sb24  , Sb25   )
+data,freq=GWBackFinder.model(z[5,:],f,idx,f_filtered,logbins_27,logbins, Sb1, Sb2  , Sb3  , Sb4  , Sb5  , Sb6  , Sb7  , Sb8  , Sb9  , Sb10  , Sb11  , Sb12  , Sb13  , Sb14  , Sb15  , Sb16  , Sb17  , Sb18  , Sb19  , Sb20  , Sb21  , Sb22  , Sb23  , Sb24  , Sb25   )
 plt.clf()
 plt.plot(freq, data[1:3970])
 xlabel("f")
 ylabel("Ω_GW")
 plt.show()
 plt.gcf()
+#%%
 
 ### Generate and save data
 @showprogress for i in 336126:500000
--- src/Data_generation/mock_signal.jl
+++ src/Data_generation/mock_signal.jl
@@ -1,5 +1,5 @@
 
-function f26(S, S0, n0, n1, n2, n3, n4, n5, n6, n7, n8, n9, n10, n11, n12, n13, n14, n15, n16, n17, n18, n19, n20, n21, n22, n23, n24, n25, As, Sb1_new3, Sb2_new3, Sb3_new3, Sb4_new3, Sb5_new3, Sb6_new3, Sb7_new3, Sb8_new3, Sb9_new3, Sb10_new3, Sb11_new3, Sb12_new3, Sb13_new3, Sb14_new3, Sb15_new3, Sb16_new3, Sb17_new3, Sb18_new3, Sb19_new3, Sb20_new3, Sb21_new3, Sb22_new3, Sb23_new3, Sb24_new3, Sb25_new3)
+function f26(S, S0, n0, n1, n2, n3, n4, n5, n6, n7, n8, n9, n10, n11, n12, n13, n14, n15, n16, n17, n18, n19, n20, n21, n22, n23, n24, n25, As, Sb1, Sb2, Sb3, Sb4, Sb5, Sb6, Sb7, Sb8, Sb9, Sb10, Sb11, Sb12, Sb13, Sb14, Sb15, Sb16, Sb17, Sb18, Sb19, Sb20, Sb21, Sb22, Sb23, Sb24, Sb25)
     """
     Function that calculates a value based on a piecewise-powerlaw expression.
 
@@ -8,96 +8,96 @@ function f26(S, S0, n0, n1, n2, n3, n4, n5, n6, n7, n8, n9, n10, n11, n12, n13, 
     - S0: Reference value.
     - n0 to n25: Exponents for various terms.
     - As: Scaling factor.
-    - Sb1_new3 to Sb25_new3: Threshold values for different ranges.
+    - Sb1 to Sb25: Threshold values for different ranges.
 
     Returns:
     - result: Calculated value based on the specified conditions.
     """
    
-    if S <= Sb1_new3
-        result = As .* (Sb12_new3 / S0).^(n12 .- n11) .* (Sb11_new3 / S0).^(n11 .- n10) .* (Sb10_new3 / S0).^(n10 .- n9) .* (Sb9_new3 / S0).^(n9 .- n8).*(Sb8_new3 / S0).^(n8 .- n7) .* (Sb7_new3 / S0).^(n7 .- n6) .* (Sb6_new3 / S0).^(n6 .- n5) .* (Sb5_new3 / S0).^(n5 .- n4) .* (Sb4_new3 / S0).^(n4 .- n3).*(Sb3_new3 / S0).^(n3 .- n2) .* (Sb2_new3 / S0).^(n2 .- n1) .* (Sb1_new3 / S0).^(n1 .- n0) .* (S / S0).^(n0)
+    if S <= Sb1
+        result = As .* (Sb12 / S0).^(n12 .- n11) .* (Sb11 / S0).^(n11 .- n10) .* (Sb10 / S0).^(n10 .- n9) .* (Sb9 / S0).^(n9 .- n8).*(Sb8 / S0).^(n8 .- n7) .* (Sb7 / S0).^(n7 .- n6) .* (Sb6 / S0).^(n6 .- n5) .* (Sb5 / S0).^(n5 .- n4) .* (Sb4 / S0).^(n4 .- n3).*(Sb3 / S0).^(n3 .- n2) .* (Sb2 / S0).^(n2 .- n1) .* (Sb1 / S0).^(n1 .- n0) .* (S / S0).^(n0)
 
-    elseif Sb1_new3 < S <= Sb2_new3
-        result = As .* (Sb12_new3 / S0).^(n12 .- n11) .* (Sb11_new3 / S0).^(n11 .- n10) .* (Sb10_new3 / S0).^(n10 .- n9) .* (Sb9_new3 / S0).^(n9 .- n8).*(Sb8_new3 / S0).^(n8 .- n7) .* (Sb7_new3 / S0).^(n7 .- n6) .* (Sb6_new3 / S0).^(n6 .- n5) .* (Sb5_new3 / S0).^(n5 .- n4) .* (Sb4_new3 / S0).^(n4 .- n3).*(Sb3_new3 / S0).^(n3 .- n2) .* (Sb2_new3 / S0).^(n2 .- n1) .* (S / S0).^(n1)
+    elseif Sb1 < S <= Sb2
+        result = As .* (Sb12 / S0).^(n12 .- n11) .* (Sb11 / S0).^(n11 .- n10) .* (Sb10 / S0).^(n10 .- n9) .* (Sb9 / S0).^(n9 .- n8).*(Sb8 / S0).^(n8 .- n7) .* (Sb7 / S0).^(n7 .- n6) .* (Sb6 / S0).^(n6 .- n5) .* (Sb5 / S0).^(n5 .- n4) .* (Sb4 / S0).^(n4 .- n3).*(Sb3 / S0).^(n3 .- n2) .* (Sb2 / S0).^(n2 .- n1) .* (S / S0).^(n1)
 
-    elseif Sb2_new3 < S <= Sb3_new3
-        result = As .* (Sb12_new3 / S0).^(n12 .- n11) .* (Sb11_new3 / S0).^(n11 .- n10) .* (Sb10_new3 / S0).^(n10 .- n9) .* (Sb9_new3 / S0).^(n9 .- n8).*(Sb8_new3 / S0).^(n8 .- n7) .* (Sb7_new3 / S0).^(n7 .- n6) .* (Sb6_new3 / S0).^(n6 .- n5) .* (Sb5_new3 / S0).^(n5 .- n4) .* (Sb4_new3 / S0).^(n4 .- n3).*(Sb3_new3 / S0).^(n3 .- n2) .* (S / S0).^(n2)
+    elseif Sb2 < S <= Sb3
+        result = As .* (Sb12 / S0).^(n12 .- n11) .* (Sb11 / S0).^(n11 .- n10) .* (Sb10 / S0).^(n10 .- n9) .* (Sb9 / S0).^(n9 .- n8).*(Sb8 / S0).^(n8 .- n7) .* (Sb7 / S0).^(n7 .- n6) .* (Sb6 / S0).^(n6 .- n5) .* (Sb5 / S0).^(n5 .- n4) .* (Sb4 / S0).^(n4 .- n3).*(Sb3 / S0).^(n3 .- n2) .* (S / S0).^(n2)
 
-    elseif Sb3_new3 < S <= Sb4_new3
-        result = As .* (Sb12_new3 / S0).^(n12 .- n11) .* (Sb11_new3 / S0).^(n11 .- n10) .* (Sb10_new3 / S0).^(n10 .- n9) .* (Sb9_new3 / S0).^(n9 .- n8).*(Sb8_new3 / S0).^(n8 .- n7) .* (Sb7_new3 / S0).^(n7 .- n6) .* (Sb6_new3 / S0).^(n6 .- n5) .* (Sb5_new3 / S0).^(n5 .- n4) .* (Sb4_new3 / S0).^(n4 .- n3).*(S / S0).^(n3)
+    elseif Sb3 < S <= Sb4
+        result = As .* (Sb12 / S0).^(n12 .- n11) .* (Sb11 / S0).^(n11 .- n10) .* (Sb10 / S0).^(n10 .- n9) .* (Sb9 / S0).^(n9 .- n8).*(Sb8 / S0).^(n8 .- n7) .* (Sb7 / S0).^(n7 .- n6) .* (Sb6 / S0).^(n6 .- n5) .* (Sb5 / S0).^(n5 .- n4) .* (Sb4 / S0).^(n4 .- n3).*(S / S0).^(n3)
 
-    elseif Sb4_new3 < S <= Sb5_new3
-        result = As .* (Sb12_new3 / S0).^(n12 .- n11) .* (Sb11_new3 / S0).^(n11 .- n10) .* (Sb10_new3 / S0).^(n10 .- n9) .* (Sb9_new3 / S0).^(n9 .- n8).*(Sb8_new3 / S0).^(n8 .- n7) .* (Sb7_new3 / S0).^(n7 .- n6) .* (Sb6_new3 / S0).^(n6 .- n5) .* (Sb5_new3 / S0).^(n5 .- n4) .* (S / S0).^(n4)
+    elseif Sb4 < S <= Sb5
+        result = As .* (Sb12 / S0).^(n12 .- n11) .* (Sb11 / S0).^(n11 .- n10) .* (Sb10 / S0).^(n10 .- n9) .* (Sb9 / S0).^(n9 .- n8).*(Sb8 / S0).^(n8 .- n7) .* (Sb7 / S0).^(n7 .- n6) .* (Sb6 / S0).^(n6 .- n5) .* (Sb5 / S0).^(n5 .- n4) .* (S / S0).^(n4)
 
-    elseif Sb5_new3 < S <= Sb6_new3
-        result = As .* (Sb12_new3 / S0).^(n12 .- n11) .* (Sb11_new3 / S0).^(n11 .- n10) .* (Sb10_new3 / S0).^(n10 .- n9) .* (Sb9_new3 / S0).^(n9 .- n8).*(Sb8_new3 / S0).^(n8 .- n7) .* (Sb7_new3 / S0).^(n7 .- n6) .* (Sb6_new3 / S0).^(n6 .- n5) .* (S / S0).^(n5)
+    elseif Sb5 < S <= Sb6
+        result = As .* (Sb12 / S0).^(n12 .- n11) .* (Sb11 / S0).^(n11 .- n10) .* (Sb10 / S0).^(n10 .- n9) .* (Sb9 / S0).^(n9 .- n8).*(Sb8 / S0).^(n8 .- n7) .* (Sb7 / S0).^(n7 .- n6) .* (Sb6 / S0).^(n6 .- n5) .* (S / S0).^(n5)
 
-    elseif Sb6_new3 < S <= Sb7_new3
-        result = As .* (Sb12_new3 / S0).^(n12 .- n11) .* (Sb11_new3 / S0).^(n11 .- n10) .* (Sb10_new3 / S0).^(n10 .- n9) .* (Sb9_new3 / S0).^(n9 .- n8).*(Sb8_new3 / S0).^(n8 .- n7) .* (Sb7_new3 / S0).^(n7 .- n6) .* (S / S0).^(n6)
+    elseif Sb6 < S <= Sb7
+        result = As .* (Sb12 / S0).^(n12 .- n11) .* (Sb11 / S0).^(n11 .- n10) .* (Sb10 / S0).^(n10 .- n9) .* (Sb9 / S0).^(n9 .- n8).*(Sb8 / S0).^(n8 .- n7) .* (Sb7 / S0).^(n7 .- n6) .* (S / S0).^(n6)
 
-    elseif Sb7_new3 < S <= Sb8_new3
-        result = As .* (Sb12_new3 / S0).^(n12 .- n11) .* (Sb11_new3 / S0).^(n11 .- n10) .* (Sb10_new3 / S0).^(n10 .- n9) .* (Sb9_new3 / S0).^(n9 .- n8).*(Sb8_new3 / S0).^(n8 .- n7) .* (S / S0).^(n7)
+    elseif Sb7 < S <= Sb8
+        result = As .* (Sb12 / S0).^(n12 .- n11) .* (Sb11 / S0).^(n11 .- n10) .* (Sb10 / S0).^(n10 .- n9) .* (Sb9 / S0).^(n9 .- n8).*(Sb8 / S0).^(n8 .- n7) .* (S / S0).^(n7)
 
-    elseif Sb8_new3 < S <= Sb9_new3
-        result = As .* (Sb12_new3 / S0).^(n12 .- n11) .* (Sb11_new3 / S0).^(n11 .- n10) .* (Sb10_new3 / S0).^(n10 .- n9) .* (Sb9_new3 / S0).^(n9 .- n8).*(S / S0).^(n8)
+    elseif Sb8 < S <= Sb9
+        result = As .* (Sb12 / S0).^(n12 .- n11) .* (Sb11 / S0).^(n11 .- n10) .* (Sb10 / S0).^(n10 .- n9) .* (Sb9 / S0).^(n9 .- n8).*(S / S0).^(n8)
 
-    elseif Sb9_new3 < S <= Sb10_new3
-        result = As .* (Sb12_new3 / S0).^(n12 .- n11) .* (Sb11_new3 / S0).^(n11 .- n10) .* (Sb10_new3 / S0).^(n10.- n9) .* (S / S0).^(n9)
+    elseif Sb9 < S <= Sb10
+        result = As .* (Sb12 / S0).^(n12 .- n11) .* (Sb11 / S0).^(n11 .- n10) .* (Sb10 / S0).^(n10.- n9) .* (S / S0).^(n9)
 
-    elseif Sb10_new3 < S <= Sb11_new3
-        result = As .* (Sb12_new3 / S0).^(n12 .- n11) .* (Sb11_new3 / S0).^(n11 .- n10) .* (S / S0).^(n10)
+    elseif Sb10 < S <= Sb11
+        result = As .* (Sb12 / S0).^(n12 .- n11) .* (Sb11 / S0).^(n11 .- n10) .* (S / S0).^(n10)
 
-    elseif Sb11_new3 < S <= Sb12_new3
-        result = As .* (Sb12_new3 / S0).^(n12 .- n11) .* (S / S0).^(n11)
+    elseif Sb11 < S <= Sb12
+        result = As .* (Sb12 / S0).^(n12 .- n11) .* (S / S0).^(n11)
 
-    elseif Sb12_new3 < S <= Sb13_new3
+    elseif Sb12 < S <= Sb13
         result = As .* (S / S0).^(n12)
 
-    elseif Sb13_new3 < S <= Sb14_new3
-        result = As .* (Sb13_new3 / S0).^(n12 .- n13) .* (S / S0).^(n13)
+    elseif Sb13 < S <= Sb14
+        result = As .* (Sb13 / S0).^(n12 .- n13) .* (S / S0).^(n13)
 
-    elseif Sb14_new3 < S <= Sb15_new3
-        result = As .* (Sb13_new3 / S0).^(n12 .- n13) .* (Sb14_new3 / S0).^(n13 .- n14) .* (S / S0).^(n14)
+    elseif Sb14 < S <= Sb15
+        result = As .* (Sb13 / S0).^(n12 .- n13) .* (Sb14 / S0).^(n13 .- n14) .* (S / S0).^(n14)
 
-    elseif Sb15_new3 < S <= Sb16_new3
-        result = As .* (Sb13_new3 / S0).^(n12 .- n13) .* (Sb14_new3 / S0).^(n13 .- n14) .* (Sb15_new3 / S0).^(n14 .- n15) .* (S / S0).^(n15)
+    elseif Sb15 < S <= Sb16
+        result = As .* (Sb13 / S0).^(n12 .- n13) .* (Sb14 / S0).^(n13 .- n14) .* (Sb15 / S0).^(n14 .- n15) .* (S / S0).^(n15)
 
-    elseif Sb16_new3 < S <= Sb17_new3
-        result = As .* (Sb13_new3 / S0).^(n12 .- n13) .* (Sb14_new3 / S0).^(n13 .- n14) .* (Sb15_new3 / S0).^(n14 .- n15) .* (Sb16_new3 / S0).^(n15 .- n16) .* (S / S0).^(n16)
+    elseif Sb16 < S <= Sb17
+        result = As .* (Sb13 / S0).^(n12 .- n13) .* (Sb14 / S0).^(n13 .- n14) .* (Sb15 / S0).^(n14 .- n15) .* (Sb16 / S0).^(n15 .- n16) .* (S / S0).^(n16)
 
-    elseif Sb17_new3 < S <= Sb18_new3
-        result = As .* (Sb13_new3 / S0).^(n12 .- n13) .* (Sb14_new3 / S0).^(n13 .- n14) .* (Sb15_new3 / S0).^(n14 .- n15) .* (Sb16_new3 / S0).^(n15 .- n16).*(Sb17_new3 / S0).^(n16 .- n17) .* (S / S0).^(n17)
+    elseif Sb17 < S <= Sb18
+        result = As .* (Sb13 / S0).^(n12 .- n13) .* (Sb14 / S0).^(n13 .- n14) .* (Sb15 / S0).^(n14 .- n15) .* (Sb16 / S0).^(n15 .- n16).*(Sb17 / S0).^(n16 .- n17) .* (S / S0).^(n17)
 
-    elseif Sb18_new3 < S <= Sb19_new3
-        result = As .* (Sb13_new3 / S0).^(n12 .- n13) .* (Sb14_new3 / S0).^(n13 .- n14) .* (Sb15_new3 / S0).^(n14 .- n15) .* (Sb16_new3 / S0).^(n15 .- n16).*(Sb17_new3 / S0).^(n16 .- n17) .* (Sb18_new3 / S0).^(n17 .- n18) .* (S / S0).^(n18)
+    elseif Sb18 < S <= Sb19
+        result = As .* (Sb13 / S0).^(n12 .- n13) .* (Sb14 / S0).^(n13 .- n14) .* (Sb15 / S0).^(n14 .- n15) .* (Sb16 / S0).^(n15 .- n16).*(Sb17 / S0).^(n16 .- n17) .* (Sb18 / S0).^(n17 .- n18) .* (S / S0).^(n18)
 
-    elseif Sb19_new3 < S <= Sb20_new3
-        result = As .* (Sb13_new3 / S0).^(n12 .- n13) .* (Sb14_new3 / S0).^(n13 .- n14) .* (Sb15_new3 / S0).^(n14 .- n15) .* (Sb16_new3 / S0).^(n15 .- n16).*(Sb17_new3 / S0).^(n16 .- n17) .* (Sb18_new3 / S0).^(n17 .- n18) .* (Sb19_new3 / S0).^(n18 .- n19) .* (S / S0).^(n19)
+    elseif Sb19 < S <= Sb20
+        result = As .* (Sb13 / S0).^(n12 .- n13) .* (Sb14 / S0).^(n13 .- n14) .* (Sb15 / S0).^(n14 .- n15) .* (Sb16 / S0).^(n15 .- n16).*(Sb17 / S0).^(n16 .- n17) .* (Sb18 / S0).^(n17 .- n18) .* (Sb19 / S0).^(n18 .- n19) .* (S / S0).^(n19)
 
-    elseif Sb20_new3 < S <= Sb21_new3
-        result = As .* (Sb13_new3 / S0).^(n12 .- n13) .* (Sb14_new3 / S0).^(n13 .- n14) .* (Sb15_new3 / S0).^(n14 .- n15) .* (Sb16_new3 / S0).^(n15 .- n16).*(Sb17_new3 / S0).^(n16 .- n17) .* (Sb18_new3 / S0).^(n17 .- n18) .* (Sb19_new3 / S0).^(n18 .- n19) .* (Sb20_new3 / S0).^(n19 .- n20) .* (S / S0).^(n20)
+    elseif Sb20 < S <= Sb21
+        result = As .* (Sb13 / S0).^(n12 .- n13) .* (Sb14 / S0).^(n13 .- n14) .* (Sb15 / S0).^(n14 .- n15) .* (Sb16 / S0).^(n15 .- n16).*(Sb17 / S0).^(n16 .- n17) .* (Sb18 / S0).^(n17 .- n18) .* (Sb19 / S0).^(n18 .- n19) .* (Sb20 / S0).^(n19 .- n20) .* (S / S0).^(n20)
 
-    elseif Sb21_new3 < S <= Sb22_new3
-        result = As .* (Sb13_new3 / S0).^(n12 .- n13) .* (Sb14_new3 / S0).^(n13 .- n14) .* (Sb15_new3 / S0).^(n14 .- n15) .* (Sb16_new3 / S0).^(n15 .- n16).*(Sb17_new3 / S0).^(n16 .- n17) .* (Sb18_new3 / S0).^(n17 .- n18) .* (Sb19_new3 / S0).^(n18 .- n19) .* (Sb20_new3 / S0).^(n19 .- n20) .* (Sb21_new3 / S0).^(n20 .- n21) .* (S / S0).^(n21)
+    elseif Sb21 < S <= Sb22
+        result = As .* (Sb13 / S0).^(n12 .- n13) .* (Sb14 / S0).^(n13 .- n14) .* (Sb15 / S0).^(n14 .- n15) .* (Sb16 / S0).^(n15 .- n16).*(Sb17 / S0).^(n16 .- n17) .* (Sb18 / S0).^(n17 .- n18) .* (Sb19 / S0).^(n18 .- n19) .* (Sb20 / S0).^(n19 .- n20) .* (Sb21 / S0).^(n20 .- n21) .* (S / S0).^(n21)
 
-    elseif Sb22_new3 < S <= Sb23_new3
-        result = As .* (Sb13_new3 / S0).^(n12 .- n13) .* (Sb14_new3 / S0).^(n13.- n14) .* (Sb15_new3 / S0).^(n14 .- n15) .* (Sb16_new3 / S0).^(n15 .- n16).*(Sb17_new3 / S0).^(n16 .- n17) .* (Sb18_new3 / S0).^(n17 .- n18) .* (Sb19_new3 / S0).^(n18 .- n19) .* (Sb20_new3 / S0).^(n19 .- n20) .*(Sb21_new3 / S0).^(n20 .- n21) .* (Sb22_new3 / S0).^(n21 .- n22) .* (S / S0).^(n22)
+    elseif Sb22 < S <= Sb23
+        result = As .* (Sb13 / S0).^(n12 .- n13) .* (Sb14 / S0).^(n13.- n14) .* (Sb15 / S0).^(n14 .- n15) .* (Sb16 / S0).^(n15 .- n16).*(Sb17 / S0).^(n16 .- n17) .* (Sb18 / S0).^(n17 .- n18) .* (Sb19 / S0).^(n18 .- n19) .* (Sb20 / S0).^(n19 .- n20) .*(Sb21 / S0).^(n20 .- n21) .* (Sb22 / S0).^(n21 .- n22) .* (S / S0).^(n22)
 
-    elseif Sb23_new3 < S <= Sb24_new3
-        result = As .* (Sb13_new3 / S0).^(n12 .- n13) .* (Sb14_new3 / S0).^(n13 .- n14) .* (Sb15_new3 / S0).^(n14 .- n15) .* (Sb16_new3 / S0).^(n15 .- n16).*(Sb17_new3 / S0).^(n16 .- n17) .* (Sb18_new3 / S0).^(n17 .- n18) .* (Sb19_new3 / S0).^(n18 .- n19) .* (Sb20_new3 / S0).^(n19 .- n20) .*(Sb21_new3 / S0).^(n20 .- n21) .* (Sb22_new3 / S0).^(n21 .- n22) .* (Sb23_new3 / S0).^(n22 .- n23) .* (S / S0).^(n23)
+    elseif Sb23 < S <= Sb24
+        result = As .* (Sb13 / S0).^(n12 .- n13) .* (Sb14 / S0).^(n13 .- n14) .* (Sb15 / S0).^(n14 .- n15) .* (Sb16 / S0).^(n15 .- n16).*(Sb17 / S0).^(n16 .- n17) .* (Sb18 / S0).^(n17 .- n18) .* (Sb19 / S0).^(n18 .- n19) .* (Sb20 / S0).^(n19 .- n20) .*(Sb21 / S0).^(n20 .- n21) .* (Sb22 / S0).^(n21 .- n22) .* (Sb23 / S0).^(n22 .- n23) .* (S / S0).^(n23)
 
-    elseif Sb24_new3 < S <= Sb25_new3
-        result = As .* (Sb13_new3 / S0).^(n12 .- n13) .* (Sb14_new3 / S0).^(n13 .- n14) .* (Sb15_new3 / S0).^(n14 .- n15) .* (Sb16_new3 / S0).^(n15 .- n16).*(Sb17_new3 / S0).^(n16 .- n17) .* (Sb18_new3 / S0).^(n17 .- n18) .* (Sb19_new3 / S0).^(n18.- n19) .* (Sb20_new3 / S0).^(n19 .- n20) .*(Sb21_new3 / S0).^(n20 .- n21) .* (Sb22_new3 / S0).^(n21 .- n22) .* (Sb23_new3 / S0).^(n22 .- n23) .* (Sb24_new3 / S0).^(n23 .- n24) .* (S / S0).^(n24)
+    elseif Sb24 < S <= Sb25
+        result = As .* (Sb13 / S0).^(n12 .- n13) .* (Sb14 / S0).^(n13 .- n14) .* (Sb15 / S0).^(n14 .- n15) .* (Sb16 / S0).^(n15 .- n16).*(Sb17 / S0).^(n16 .- n17) .* (Sb18 / S0).^(n17 .- n18) .* (Sb19 / S0).^(n18.- n19) .* (Sb20 / S0).^(n19 .- n20) .*(Sb21 / S0).^(n20 .- n21) .* (Sb22 / S0).^(n21 .- n22) .* (Sb23 / S0).^(n22 .- n23) .* (Sb24 / S0).^(n23 .- n24) .* (S / S0).^(n24)
  
-    else S > Sb25_new3
-        result = As .* (Sb13_new3 / S0).^(n12 .- n13) .* (Sb14_new3 / S0).^(n13 .- n14) .* (Sb15_new3 / S0).^(n14 .- n15) .* (Sb16_new3 / S0).^(n15 .- n16).*(Sb17_new3 / S0).^(n16 .- n17) .* (Sb18_new3 / S0).^(n17 .- n18) .* (Sb19_new3 / S0).^(n18 .- n19) .* (Sb20_new3 / S0).^(n19 .- n20).*(Sb21_new3 / S0).^(n20 .- n21) .* (Sb22_new3 / S0).^(n21 .- n22) .* (Sb23_new3 / S0).^(n22 .- n23) .* (Sb24_new3 / S0).^(n23 .- n24).*(Sb25_new3 / S0).^(n24 .- n25) .* (S / S0).^(n25)
+    else S > Sb25
+        result = As .* (Sb13 / S0).^(n12 .- n13) .* (Sb14 / S0).^(n13 .- n14) .* (Sb15 / S0).^(n14 .- n15) .* (Sb16 / S0).^(n15 .- n16).*(Sb17 / S0).^(n16 .- n17) .* (Sb18 / S0).^(n17 .- n18) .* (Sb19 / S0).^(n18 .- n19) .* (Sb20 / S0).^(n19 .- n20).*(Sb21 / S0).^(n20 .- n21) .* (Sb22 / S0).^(n21 .- n22) .* (Sb23 / S0).^(n22 .- n23) .* (Sb24 / S0).^(n23 .- n24).*(Sb25 / S0).^(n24 .- n25) .* (S / S0).^(n25)
 
     end
     return result
 end
 
 
- function model(z1,f,idx,f_filtered,logbins_26,logbins,Sb1_new3, Sb2_new3, Sb3_new3, Sb4_new3, Sb5_new3, Sb6_new3, Sb7_new3, Sb8_new3, Sb9_new3, Sb10_new3, Sb11_new3, Sb12_new3, Sb13_new3, Sb14_new3, Sb15_new3, Sb16_new3, Sb17_new3, Sb18_new3, Sb19_new3, Sb20_new3, Sb21_new3, Sb22_new3, Sb23_new3, Sb24_new3, Sb25_new3)
+ function model(z1,f,idx,f_filtered,logbins_26,logbins,Sb1, Sb2, Sb3, Sb4, Sb5, Sb6, Sb7, Sb8, Sb9, Sb10, Sb11, Sb12, Sb13, Sb14, Sb15, Sb16, Sb17, Sb18, Sb19, Sb20, Sb21, Sb22, Sb23, Sb24, Sb25)
     """
     Model function that generates data based on given parameters.
 
@@ -108,7 +108,7 @@ end
     - f_filtered: Filtered frequency vector.
     - logbins_26: Edges of the 26 bins.
     - logbins: Edges of the 1000 bins .
-    - Sb1_new3 to Sb25_new3: Threshold values where the powerlaw changes.
+    - Sb1 to Sb25: Threshold values where the powerlaw changes.
 
     Returns:
     - Data_total: Combined data vector.
@@ -122,7 +122,7 @@ end
     z3 = z1[28]
 
     # Signal part. Following Data generation described in https://arxiv.org/pdf/2009.11845.pdf
-    stds_omega = [sqrt.(f26(fi, logbins_26[13], z[1], z[2], z[3], z[4], z[5], z[6], z[7], z[8], z[9], z[10], z[11], z[12], z[13], z[14], z[15], z[16], z[17], z[18], z[19], z[20], z[21], z[22], z[23], z[24], z[25], z[26], 10^(z2), Sb1_new3, Sb2_new3, Sb3_new3, Sb4_new3, Sb5_new3, Sb6_new3, Sb7_new3, Sb8_new3, Sb9_new3, Sb10_new3, Sb11_new3, Sb12_new3, Sb13_new3, Sb14_new3, Sb15_new3, Sb16_new3, Sb17_new3, Sb18_new3, Sb19_new3, Sb20_new3, Sb21_new3, Sb22_new3, Sb23_new3, Sb24_new3, Sb25_new3)) for fi in f]
+    stds_omega = [sqrt.(f26(fi, logbins_26[13], z[1], z[2], z[3], z[4], z[5], z[6], z[7], z[8], z[9], z[10], z[11], z[12], z[13], z[14], z[15], z[16], z[17], z[18], z[19], z[20], z[21], z[22], z[23], z[24], z[25], z[26], 10^(z2), Sb1, Sb2, Sb3, Sb4, Sb5, Sb6, Sb7, Sb8, Sb9, Sb10, Sb11, Sb12, Sb13, Sb14, Sb15, Sb16, Sb17, Sb18, Sb19, Sb20, Sb21, Sb22, Sb23, Sb24, Sb25)) for fi in f]
     stds_omega_cuda = CuArray(Float32.(stds_omega))
 
     CUDA.device_reset!
@@ -145,7 +145,6 @@ end
 
     std_eps = CUDA.randn(Float32, length(stds_n), 94)
     CUDA.@sync samples_noise4 = stds_n_cuda .* std_eps
-
 
     CUDA.@sync c1 = (samples_noise3 .^ 2 + samples_noise4 .^ 2) / 2
 
