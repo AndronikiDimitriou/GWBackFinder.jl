10439c94579112b37082fc059d6cb99ac1a6c8b9
AndronikiDimitriou <androniki.dimitriou@ific.uv.es>
2024-01-12 01:37:46 +0100

    "up"

--- examples/generate_mock_signal.jl
+++ examples/generate_mock_signal.jl
@@ -19,7 +19,8 @@ idx,idx26,logbins_26,logbins,f_filtered=GWBackFinder.binning(f)
 Sb1, Sb2, Sb3, Sb4, Sb5, Sb6, Sb7, Sb8, Sb9, Sb10, Sb11, Sb12, Sb13, Sb14, Sb15, Sb16, Sb17, Sb18, Sb19, Sb20, Sb21, Sb22, Sb23, Sb24, Sb25, Sb26= logbins_26[1], logbins_26[2], logbins_26[3], logbins_26[4], logbins_26[5], logbins_26[6], logbins_26[7], logbins_26[8], logbins_26[9], logbins_26[10], logbins_26[11], logbins_26[12], logbins_26[13], logbins_26[14], logbins_26[15], logbins_26[16], logbins_26[17], logbins_26[18], logbins_26[19], logbins_26[20], logbins_26[21], logbins_26[22], logbins_26[23], logbins_26[24], logbins_26[25], logbins_26[26]
 
 ### Define values for parameters randomly. 10000000 different z (z -> 27 slopes, 1 amplitude, 1 noise parameter A) 
-z= npzread("/data/users/Androniki/Dani/z_new.npy") #load parameters generated using sbi
+z= npzread("./z_new.npy") #download dataset from Kaggle https://www.kaggle.com/andronikidimitriou/datasets generated using sbi and load parameters 
+#z= npzread("/data/users/Androniki/Dani/z_new.npy") #load parameters generated using sbi
 #z=rand(1000000,29) # for example uniform prior
 
 ### Look at an example and plot it 
